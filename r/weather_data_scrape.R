c19437a39ba4899cb225b5d31786c370d5c09ded
micah <micah@asianturfgrass.com>
2015-01-01 08:59:36 +0900

    added data for NY (JFK)

--- r/weather_data_scrape.R
+++ r/weather_data_scrape.R
@@ -75,6 +75,21 @@ itm_2013 <- read.csv('http://www.wunderground.com/history/airport/RJOO/2013/1/1/
 itm_2014 <- read.csv('http://www.wunderground.com/history/airport/RJOO/2014/1/1/CustomHistory.html?dayend=31&monthend=12&yearend=2014&req_city=NA&req_state=NA&req_statename=NA%22&format=1',
                      header = TRUE)
 
+jfk_2010 <- read.csv('http://www.wunderground.com/history/airport/KJFK/2010/1/1/CustomHistory.html?dayend=31&monthend=12&yearend=2010&req_city=NA&req_state=NA&req_statename=NA%22&format=1',
+                     header = TRUE)
+
+jfk_2011 <- read.csv('http://www.wunderground.com/history/airport/KJFK/2011/1/1/CustomHistory.html?dayend=31&monthend=12&yearend=2011&req_city=NA&req_state=NA&req_statename=NA%22&format=1',
+                     header = TRUE)
+
+jfk_2012 <- read.csv('http://www.wunderground.com/history/airport/KJFK/2012/1/1/CustomHistory.html?dayend=31&monthend=12&yearend=2012&req_city=NA&req_state=NA&req_statename=NA%22&format=1',
+                     header = TRUE)
+
+jfk_2013 <- read.csv('http://www.wunderground.com/history/airport/KJFK/2013/1/1/CustomHistory.html?dayend=31&monthend=12&yearend=2013&req_city=NA&req_state=NA&req_statename=NA%22&format=1',
+                     header = TRUE)
+
+jfk_2014 <- read.csv('http://www.wunderground.com/history/airport/KJFK/2014/1/1/CustomHistory.html?dayend=31&monthend=12&yearend=2014&req_city=NA&req_state=NA&req_statename=NA%22&format=1',
+                     header = TRUE)
+
 athens <- as.data.frame(rbind(athens_2010, athens_2011, 
                               athens_2012, athens_2013,
                               athens_2014))
@@ -95,17 +110,23 @@ osaka <- as.data.frame(rbind(itm_2010, itm_2011,
                              itm_2012, itm_2013,
                              itm_2014))
 
+ny <- as.data.frame(rbind(jfk_2010, jfk_2011,
+                          jfk_2012, jfk_2013,
+                          jfk_2014))
+
 athens$year <- year(as.character(athens$EST))
 tokyo$year <- year(as.character(tokyo$JST))
 portland$year <- year(as.character(portland$PST))
 knoxville$year <- year(as.character(knoxville$EST))
 osaka$year <- year(as.character(osaka$JST))
+ny$year <- year(as.character(ny$EST))
 
 athens$city <- "Athens, GA, USA"
 tokyo$city <- "Tokyo, Japan"
 portland$city <- "Portland, OR, USA"
 knoxville$city <- "Knoxville, TN, USA"
 osaka$city <- "Osaka, Japan"
+ny$city <- "New York, NY, USA"
 
 athens <- cbind(athens$Min.TemperatureC, athens$year, athens$city, 
                 athens$Mean.TemperatureC)
@@ -117,11 +138,15 @@ knoxville <- cbind(knoxville$Min.TemperatureC, knoxville$year,
                    knoxville$city, knoxville$Mean.TemperatureC)
 osaka <- cbind(osaka$Min.TemperatureC, osaka$year, osaka$city,
                osaka$Mean.TemperatureC)
+ny <- cbind(ny$Min.TemperatureC, ny$year, ny$city, ny$Mean.TemperatureC)
 
 # write the output to the data folder so that these analyses
 # can be completed and charts generated without internet access
+
 # write.table(athens, "data/athens.csv", sep = ",", row.names = FALSE)
 # write.table(tokyo, "data/tokyo.csv", sep = ",", row.names = FALSE)
 # write.table(portland, "data/portland.csv", sep = ",", row.names = FALSE)
 # write.table(knoxville, "data/knoxville.csv", sep = ",", row.names = FALSE)
 # write.table(osaka, "data/osaka.csv", sep = ",", row.names = FALSE)
+# write.table(ny, "data/ny.csv", sep = ",", row.names = FALSE)
+
