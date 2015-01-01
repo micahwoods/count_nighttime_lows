6e94e9d533875c16e4c0899eaa451c98e5c17930
micah <micah@asianturfgrass.com>
2015-01-01 09:00:32 +0900

    fixed consistent factor colors by city in multiple charts

--- 20141231_low_temps.R
+++ 20141231_low_temps.R
@@ -6,7 +6,7 @@ source("r/weather_data_scrape.R")
 # the data folder contains the results of "r/weather_data_scrape.R"
 
 # combine the data from multiple cities
-all <- as.data.frame(rbind(athens, tokyo, portland, knoxville, osaka))
+all <- as.data.frame(rbind(athens, tokyo, portland, knoxville, osaka, ny))
 
 # give simple column names and format data
 colnames(all) <- c("low", "year", "city", "meanTemp")
@@ -42,7 +42,8 @@ colnames(count24) <- c("city", "year", "count")
 p <- ggplot(data = count18, aes(x = year, y = count))
 p + geom_point(aes(colour = city), size = 4) +
   geom_line(aes(colour = city), size = 0.2) +
-  scale_colour_brewer(palette = "Set1") +
+  scale_colour_brewer(palette = "Set1",
+                      drop = TRUE, limits = levels(count18$city)) +
   scale_y_continuous(limits = c(0, max(count18$count))) +
   labs(x = "Year",
        y = "Number of days with low temperature > 18°C (65°F)") +
@@ -51,9 +52,11 @@ p + geom_point(aes(colour = city), size = 4) +
 
 # plot this as city & year for low temps > 21C
 p <- ggplot(data = count21, aes(x = year, y = count))
-p + geom_point(aes(colour = city), size = 4) +
+p + geom_point(aes(colour = city, drop = TRUE), size = 4) +
   geom_line(aes(colour = city), size = 0.2) +
-  scale_colour_brewer(palette = "Set1") +
+  scale_colour_brewer(palette = "Set1",
+                      limits = levels(count21$city),
+                      drop = TRUE) +
   scale_y_continuous(limits = c(0, max(count21$count))) +
   labs(x = "Year",
        y = "Number of days with low temperature > 21°C (70°F)") +
@@ -64,7 +67,8 @@ p + geom_point(aes(colour = city), size = 4) +
 p <- ggplot(data = count24, aes(x = year, y = count))
 p + geom_point(aes(colour = city), size = 4) +
   geom_line(aes(colour = city), size = 0.2) +
-  scale_colour_brewer(palette = "Set1") +
+  scale_colour_brewer(palette = "Set1",
+                      drop = TRUE, limits = levels(count24$city)) +
   scale_y_continuous(limits = c(0, max(count24$count))) +
   labs(x = "Year",
        y = "Number of days with low temperature > 24°C (75°F)") +
