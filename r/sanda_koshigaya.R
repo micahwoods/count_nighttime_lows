6e3273c2028753ad7528c9b525d079dafca31da5
micah <micah@asianturfgrass.com>
2015-01-07 21:07:09 +0900

    reads Sanda (Hyogo) data for 5 years

--- r/sanda_koshigaya.R
+++ r/sanda_koshigaya.R
@@ -7,13 +7,14 @@
 # set month as MM
 
 sanda_base <- "http://www.data.jma.go.jp/obd/stats/etrn/view/daily_a1.php?prec_no=63&block_no=0969&year=XXXX&month=MM&day=&view="
+koshigaya_base <- "http://www.data.jma.go.jp/obd/stats/etrn/view/daily_a1.php?prec_no=43&block_no=0364&year=XXXX&month=MM&day=1&view="
 
 sanda <- vector(mode ="character", length(5))
 
 for (i in 1:5) {
   year <- 2009 + i
   address <- gsub("XXXX", year, sanda_base)
-  sanda[[i]] <- address 
+  sanda[[i]] <- address
 }
 
 results <- data.frame()
@@ -24,49 +25,35 @@ for(j in (1:12)) {
     results <- rbind(results, newline)
   }
 
+year <- rep(2010:2014, times = 12)
+
 results$address <- as.character(results$address)
-results$j <- as.numeric(as.character(results$j))
+results$month <- as.numeric(as.character(results$j))
+results$year <- year
 
 sanda5years <- data.frame()
 
-for (i in 1:4) {
+for (i in 1:60) {
   sandaTables <- readHTMLTable(results[[i, 2]])
-  month <- sandaTables[[2]]
-  month$count <- i
-  month$ticker.month <- results[[i, 1]]
-  sanda5years <- rbind(sanda5years, month)
+  weather.data <- sandaTables[[2]]
+  weather.data$month <- results[[i, 1]]
+  weather.data$year <- results[[i, 4]]
+  sanda5years <- rbind(sanda5years, weather.data)
 }
 
 # clean the data
-full.sanda <- as.data.frame(sanda5years[, c(1, 5, 6, 7, 17)])
-colnames(full.sanda) <- c("date", "mean.temp", "high", "low", "count")
-full.sanda$date <- as.numeric(as.character(full.sanda$date), as.is = TRUE)
+full.sanda <- as.data.frame(sanda5years[, c(1, 5, 6, 7, 17, 18)])
+colnames(full.sanda) <- c("day", "mean.temp", "high", "low", "month", "year")
+full.sanda$day <- as.numeric(as.character(full.sanda$day), as.is = TRUE)
 full.sanda$mean.temp <- as.numeric(as.character(full.sanda$mean.temp), as.is = TRUE)
 full.sanda$low <- as.numeric(as.character(full.sanda$low), as.is = TRUE)
 full.sanda$high <- as.numeric(as.character(full.sanda$high), as.is = TRUE)
-
-# make vector to figure out what is missing
-original <- full.sanda$date
+full.sanda$month <- as.numeric(as.character(full.sanda$month))
 
 # remove the not necessary rows
 full.sanda <- subset(full.sanda, mean.temp > -279)
 
-after <- full.sanda$date
+# paste date to 1 variable
+full.sanda$date <- ymd(paste(full.sanda$year, full.sanda$month,
+                             full.sanda$day, sep = "-"))
 
-original[!(original %in% after)]
-
-# make a date vector of 2010-1-1 to 2014-12-31
-days <- as.numeric(length(full.sanda$mean.temp))
-add.vector <- c(1:(days))
-start <- ymd("2010-01-01")
-daily.vector <- start + (days(add.vector) - 1)
-
-# can figure this out by days, pasted, then check for match
-# for now quick plot and count
-
-full.sanda <- as.data.frame(cbind(full.sanda, daily.vector))
-
-p <- ggplot(data = full.sanda, aes(x = daily.vector, y = low))
-p + geom_point()
-
-# to clean the data, just merge 2 columns and then sort by them
