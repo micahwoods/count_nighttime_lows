f00df3a515301616d2e75da22a6c069e29d2ad39
micah <micah@asianturfgrass.com>
2015-01-07 21:15:12 +0900

    reads sanda or koshigaya based on search and replace

--- r/sanda_koshigaya.R
+++ r/sanda_koshigaya.R
@@ -9,18 +9,18 @@
 sanda_base <- "http://www.data.jma.go.jp/obd/stats/etrn/view/daily_a1.php?prec_no=63&block_no=0969&year=XXXX&month=MM&day=&view="
 koshigaya_base <- "http://www.data.jma.go.jp/obd/stats/etrn/view/daily_a1.php?prec_no=43&block_no=0364&year=XXXX&month=MM&day=1&view="
 
-sanda <- vector(mode ="character", length(5))
+koshigaya <- vector(mode ="character", length(5))
 
 for (i in 1:5) {
   year <- 2009 + i
-  address <- gsub("XXXX", year, sanda_base)
-  sanda[[i]] <- address
+  address <- gsub("XXXX", year, koshigaya_base)
+  koshigaya[[i]] <- address
 }
 
 results <- data.frame()
 
 for(j in (1:12)) {
-    address <- gsub("MM", j, sanda)
+    address <- gsub("MM", j, koshigaya)
     newline <- cbind(j, address)
     results <- rbind(results, newline)
   }
@@ -31,29 +31,31 @@ results$address <- as.character(results$address)
 results$month <- as.numeric(as.character(results$j))
 results$year <- year
 
-sanda5years <- data.frame()
+koshigaya5years <- data.frame()
 
 for (i in 1:60) {
-  sandaTables <- readHTMLTable(results[[i, 2]])
-  weather.data <- sandaTables[[2]]
+  koshigayaTables <- readHTMLTable(results[[i, 2]])
+  weather.data <- koshigayaTables[[2]]
   weather.data$month <- results[[i, 1]]
   weather.data$year <- results[[i, 4]]
-  sanda5years <- rbind(sanda5years, weather.data)
+  koshigaya5years <- rbind(koshigaya5years, weather.data)
 }
 
 # clean the data
-full.sanda <- as.data.frame(sanda5years[, c(1, 5, 6, 7, 17, 18)])
-colnames(full.sanda) <- c("day", "mean.temp", "high", "low", "month", "year")
-full.sanda$day <- as.numeric(as.character(full.sanda$day), as.is = TRUE)
-full.sanda$mean.temp <- as.numeric(as.character(full.sanda$mean.temp), as.is = TRUE)
-full.sanda$low <- as.numeric(as.character(full.sanda$low), as.is = TRUE)
-full.sanda$high <- as.numeric(as.character(full.sanda$high), as.is = TRUE)
-full.sanda$month <- as.numeric(as.character(full.sanda$month))
+full.koshigaya <- as.data.frame(koshigaya5years[, c(1, 5, 6, 7, 17, 18)])
+colnames(full.koshigaya) <- c("day", "mean.temp", "high", "low", "month", "year")
+full.koshigaya$day <- as.numeric(as.character(full.koshigaya$day), as.is = TRUE)
+full.koshigaya$mean.temp <- as.numeric(as.character(full.koshigaya$mean.temp), as.is = TRUE)
+full.koshigaya$low <- as.numeric(as.character(full.koshigaya$low), as.is = TRUE)
+full.koshigaya$high <- as.numeric(as.character(full.koshigaya$high), as.is = TRUE)
+full.koshigaya$month <- as.numeric(as.character(full.koshigaya$month))
 
 # remove the not necessary rows
-full.sanda <- subset(full.sanda, mean.temp > -279)
+full.koshigaya <- subset(full.koshigaya, mean.temp > -279)
 
 # paste date to 1 variable
-full.sanda$date <- ymd(paste(full.sanda$year, full.sanda$month,
-                             full.sanda$day, sep = "-"))
+full.koshigaya$date <- ymd(paste(full.koshigaya$year, full.koshigaya$month,
+                             full.koshigaya$day, sep = "-"))
 
+p <- ggplot(data = full.koshigaya, aes(x = date, y = low))
+p + geom_point()
