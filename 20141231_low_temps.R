05ee0036e594ea474e6a0c7fe68f9a45b44b9fda
micah <micah@asianturfgrass.com>
2015-01-07 23:09:02 +0900

    reads the updated data files and summarizes

--- 20141231_low_temps.R
+++ 20141231_low_temps.R
@@ -1,27 +1,40 @@
 # get low temperature data for various cities for 2010 to 2014
 # and plot them
 
+# load required packages
 source("r/utilities.R")
-source("r/weather_data_scrape.R")
 
+# this scrapes airport data from Weather Underground
 # the data folder contains the results of "r/weather_data_scrape.R"
+# source("r/weather_data_scrape.R")
 
-# combine the data from multiple cities
-# Portland data downloaded but not included here
-all <- as.data.frame(rbind(athens, tokyo, knoxville, 
-                           osaka, ny, jax))
+# read Carefree Arizona data
+# and also this contains Jacksonville, Augusta, TYS, & Farmingdale
+source("r/carefree.R")
+
+# read Sydney data
+source("r/sydney.R")
+
+# read Japan data
+# sourcing r/sanda_koshigaya.R would download all the data again from JMA
+# this just reads in what has already been downloaded by r/sanda_koshigaya.R
+source("r/japan.R")
+
+# now one can bind all these data from various cities into one data fram
+
+
+all <- as.data.frame(rbind(carefree, tys, jb, aug,
+                           ny, syd, koshigaya, sanda))
 
 # give simple column names and format data
-colnames(all) <- c("low", "year", "city", "meanTemp")
-all$low <- as.numeric(as.character(all$low))
-all$year <- as.numeric(as.character(all$year))
-all$meanTemp <- as.numeric(as.character(all$meanTemp))
+colnames(all) <- c("low", "year", "city", "meanTemp", "high", "date")
                        
 # select the cutoff temperature
 # 65F is 18.3C, 70F is 21.1C, 75F is 23.9C
 low18 <- filter(all, low > 18)
 low21 <- filter(all, low > 21)
 low23 <- filter(all, low > 23)
+low24 <- filter(all, low > 24)
 
 # generate a dataframe that has counts of days per year 
 # per city above the cutoff temperature
@@ -37,9 +50,14 @@ count23 <- low23 %>%
   group_by(city, year) %>%
   summarise(length(low))
 
+count24 <- low24 %>%
+  group_by(city, year) %>%
+  summarise(length(low))
+
 colnames(count18) <- c("city", "year", "count")
 colnames(count21) <- c("city", "year", "count")
 colnames(count23) <- c("city", "year", "count")
+colnames(count24) <- c("city", "year", "count")
 
 # plot this as city & year for low temps > 18C
 p <- ggplot(data = count18, aes(x = year, y = count))
@@ -113,15 +131,18 @@ colnames(meanLow23) <- c("city", "average")
 
 meanLow23$order.city <- reorder(meanLow23$city, meanLow23$average)
 
+# working here with lows of 23 at moment
 p <- ggplot(data = meanLow23, aes(x = order.city, y = average, label = average))
-p + geom_point(size = 4, aes(colour = city)) +
+p + geom_point(size = 4) +
   scale_y_continuous(limits = c(0, (max(meanLow23$average) + 10))) +
   coord_flip() + 
-  geom_text(hjust= -0.5, vjust=0, aes(colour = city)) +
+  geom_text(hjust= -0.5, vjust=0) +
   labs(y = "Mean # of days with low temperature > 23°C (73°F), 2010 to 2014",
        x = "City") +
-  scale_colour_brewer(palette = "Set1",
-                      drop = TRUE, limits = levels(count23$city)) +
-  guides(colour = guide_legend(title=NULL, drop = TRUE)) +
   theme_minimal(base_size = 18)
 
+combined <- filter(all, low > 23 & high > 30)
+
+combo <- combined %>%
+  group_by(city, year) %>%
+  summarise(length(low))
