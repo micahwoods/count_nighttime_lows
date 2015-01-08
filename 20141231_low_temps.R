82353f4d5b2fb38d2011225b43610f95f15c59b8
micah <micah@asianturfgrass.com>
2015-01-08 12:14:13 +0900

    a few more charts and chart formatting tweaks

--- 20141231_low_temps.R
+++ 20141231_low_temps.R
@@ -69,7 +69,7 @@ p + geom_point(aes(colour = city), size = 4) +
   labs(x = "Year",
        y = "Number of days with low temperature > 18°C (65°F)") +
   guides(colour = guide_legend(title=NULL)) +
-  theme_minimal(base_size = 18)
+  theme_minimal(base_size = 22)
 
 # plot this as city & year for low temps > 21C
 p <- ggplot(data = count21, aes(x = year, y = count))
@@ -81,7 +81,7 @@ p + geom_point(aes(colour = city), size = 4) +
   labs(x = "Year",
        y = "Number of days with low temperature > 21°C (70°F)") +
   guides(colour = guide_legend(title=NULL)) +
-  theme_minimal(base_size = 18)
+  theme_minimal(base_size = 22)
 
 # plot this as city & year for low temps > 23C
 p <- ggplot(data = count23, aes(x = year, y = count))
@@ -93,7 +93,7 @@ p + geom_point(aes(colour = city), size = 4) +
   labs(x = "Year",
        y = "Number of days with low temperature > 23°C (73°F)") +
   guides(colour = guide_legend(title=NULL)) +
-  theme_minimal(base_size = 18)
+  theme_minimal(base_size = 22)
 
 # make a dataframe of mean annual temperature
 # by city and by year, and see if summer length
@@ -101,9 +101,9 @@ p + geom_point(aes(colour = city), size = 4) +
 
 annualMean <- all %>%
   group_by(city, year) %>%
-  summarise(mean(meanTemp))
+  summarise(mean(meanTemp, na.rm = TRUE))
 
-low_and_mean <- merge(count18, annualMean)
+low_and_mean <- merge(count23, annualMean)
 colnames(low_and_mean) <- c("city", "year", "count", "meanTemp")
 
 # plot summer length as count of days with low > X vs annual meanTemp
@@ -113,10 +113,10 @@ p + geom_point(aes(colour = city), size = 4) +
                                 max(low_and_mean$meanTemp) + 1)) +
   scale_colour_brewer(palette = "Set1") +
   labs(x = "Mean annual temperature (°C)",
-       y = "Number of days with low temperature > 18°C (65°F)") +
-  geom_text(hjust= -0.2, vjust=0, size = 3.5, aes(colour = city)) +
+       y = "Number of days with low temperature > 23°C (73°F)") +
+  geom_text(hjust= -0.2, vjust=0, size = 4, aes(colour = city)) +
   guides(colour = guide_legend(title=NULL)) +
-  theme_minimal(base_size = 18)
+  theme_minimal(base_size = 22)
 
 # get mean annual lows > 23 over 5 years (2010 to 2014)
 # as an index of bentgrass damaging summer length
@@ -132,17 +132,44 @@ colnames(meanLow23) <- c("city", "average")
 meanLow23$order.city <- reorder(meanLow23$city, meanLow23$average)
 
 # working here with lows of 23 at moment
-p <- ggplot(data = meanLow23, aes(x = order.city, y = average, label = average))
+p <- ggplot(data = meanLow23, aes(x = order.city, y = average, label = round(average)))
 p + geom_point(size = 4) +
   scale_y_continuous(limits = c(0, (max(meanLow23$average) + 10))) +
   coord_flip() + 
   geom_text(hjust= -0.5, vjust=0) +
   labs(y = "Mean # of days with low temperature > 23°C (73°F), 2010 to 2014",
        x = "City") +
-  theme_minimal(base_size = 18)
+  theme_minimal(base_size = 20)
 
-combined <- filter(all, low > 23 & high > 30)
+combined <- filter(all, low > 23 & high >= 30)
+notHot <- filter(all, low <= 23 & high >= 30)
+relief <- filter(all, low <= 21 & high >= 30)
+mild <- filter(all, low <= 18 & high >= 30)
 
-combo <- combined %>%
+mildSummary <- mild%>%
   group_by(city, year) %>%
-  summarise(length(low))
+  summarise((length(low)))
+
+colnames(mildSummary) <- c("city", "year", "count")
+
+# plot this as city & year for low temps > 23C & high >= 30
+p <- ggplot(data = mildSummary, aes(x = year, y = count))
+p + geom_point(aes(colour = city), size = 4) +
+  geom_line(aes(colour = city), size = 0.2) +
+  scale_colour_brewer(palette = "Set1",
+                      drop = TRUE, limits = levels(mildSummary$city)) +
+  scale_y_continuous(limits = c(0, max(mildSummary$count))) +
+  labs(x = "Year",
+       y = "# of days with low <= 18°C & high >= 30°C, 2010 to 2014") +
+  guides(colour = guide_legend(title=NULL)) +
+  theme_minimal(base_size = 22)
+
+
+
+hot <- filter(all, city == "Carefree, AZ, USA" &
+                high >= 40)
+
+summary(hot$low)
+hist(hot$low)
+
+filter(all, date == jan)
